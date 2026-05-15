e34ea1d68d2adebdf37fdcde4cca64fe3078d662
radhika29 <radhika@techxers.com>
2026-05-15 20:13:55 +0530

    yum kart

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.example.yum_kart"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23  // Firebase Auth requires minSdk >= 23
+        minSdk = flutter.minSdkVersion  // Firebase Auth requires minSdk >= 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
